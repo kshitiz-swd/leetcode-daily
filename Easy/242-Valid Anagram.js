6a6510260d4247df34af196a98bd4407e850e8b6
kshitiz-swd <kshitiz123srivastava@gmail.com>
2025-07-02 12:46:45 +0530

    Create 242-Valid Anagram.js
    Days-2

---
+++ Easy/242-Valid Anagram.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    let mapS = {}
+    let mapT = {}
+
+    if(s.length !== t.length) return false
+
+    for(let i = 0; i < s.length;i++){
+        mapS[s[i]] = (mapS[s[i]] || 0) + 1
+        mapT[t[i]] = (mapT[t[i]] || 0) + 1
+    }
+    console.log(mapS)
+    console.log(mapT)
+
+    for(let key in mapS){
+        if(mapS[key] !== mapT[key]) return false
+    }
+    return true
+};
